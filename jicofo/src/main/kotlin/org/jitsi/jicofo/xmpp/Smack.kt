5b263ae80842bf7c038af5bc5f6a23383a5d7f07
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2026-07-07 12:35:13 +0200

    Remove manual TraceParentProvider register

--- jicofo/src/main/kotlin/org/jitsi/jicofo/xmpp/Smack.kt
+++ jicofo/src/main/kotlin/org/jitsi/jicofo/xmpp/Smack.kt
@@ -18,8 +18,6 @@
 package org.jitsi.jicofo.xmpp
 
 import org.jitsi.xmpp.extensions.DefaultPacketExtensionProvider
-import org.jitsi.xmpp.extensions.TraceParent
-import org.jitsi.xmpp.extensions.TraceParentProvider
 import org.jitsi.xmpp.extensions.colibri.ColibriStatsIqProvider
 import org.jitsi.xmpp.extensions.colibri.ForcefulShutdownIqProvider
 import org.jitsi.xmpp.extensions.colibri.GracefulShutdownIqProvider
@@ -164,9 +162,4 @@ fun registerXmppExtensions() {
         DefaultPacketExtensionProvider(VideoMutedExtension::class.java)
     )
     IqProviderUtils.registerProviders()
-    ProviderManager.addExtensionProvider(
-        TraceParent.ELEMENT,
-        TraceParent.NAMESPACE,
-        TraceParentProvider()
-    )
 }
